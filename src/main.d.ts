fe8c1235d74a8177c1ec8e7094063d6f86688549
imjano <imjano@outlook.com>
2023-05-11 22:45:09 -0600

    update readme.md

---
+++ src/main.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @author imjano
+ * @param obj1
+ * @param obj2
+ * @returns boolean
+ * @description This function compare deeply 2 objects
+ * returns true if the objects are identical
+ */
+declare function compareObjects(obj1: any, obj2: any): boolean
+export = compareObjects
